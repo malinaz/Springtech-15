6a8ef6d246942dc61deb4018a494dda153ee1b94
catalintaur2009 <37048874+catalintaur2009@users.noreply.github.com>
2020-05-06 11:20:33 +0300

    post&comm

---
+++ server/Posts/post.datastore.js
@@ -0,0 +1,57 @@
+const Post = require('./post.model');
+
+const postsDatastore = {
+    createPost: (value, success, fail) => {
+        Post.create(value)
+            .then((data) => {
+                success(data);
+            }).catch((error) => {
+            fail(error);
+        });
+    },
+
+    updatePost: (id, value, success, fail) => {
+        Post.findOneAndUpdate(
+            {"_id": id},
+            {$set: {"text": value}},
+            {new: true})
+            .then((data) => {
+                success(data);
+            }).catch((error) => {
+            fail(error);
+        });
+    },
+
+    getAllPosts: (id, success, fail) => {
+        Post.find({"userId": id})
+            .then((data) => {
+                success(data);
+            }).catch((error) => {
+            fail(error);
+        });
+    },
+
+    removePost: (id, success, fail) => {
+        Post.deleteOne({"_id": id})
+            .then((data) => {
+                success(data);
+            }).catch((error) => {
+            fail(error);
+        });
+    }
+
+
+
+
+
+    /*findComments: (id, success, fail) => {
+        Post.findById(id)
+            .then((data) => {
+                success(data);
+            }).catch((error) => {
+            fail(error);
+        });
+    }*/
+}
+
+module.exports = postsDatastore;
