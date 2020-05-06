e2c072f3a78d6bc5d680257de035bcbd9ad93380
TeodoraMM <teodora_mezei12@yahoo.com>
2020-05-06 20:11:03 +0300

    front-end feed

--- server/client/feed/feed.js
+++ server/client/feed/feed.js
@@ -10,6 +10,7 @@ function buildMenu() {
   $("#container").append(menu);
 }
 
+//aray with posts
 let posts = [
   {
     text: "LALALALAL LALLALALAL LAAAAAAAAAAAAAAAAAAA",
@@ -67,6 +68,7 @@ const postsList = $("<ul>", "</ul>");
 postsList.addClass("posts-list");
 
 function renderPostsList() {
+  //create new list element for every post
   for (let i = 0; i < posts.length; i++) {
     postsList.append(createNewPost(posts[i]));
   }
@@ -82,7 +84,7 @@ function createNewPost(post) {
   postText.addClass("text-post");
   postText.text(post.text);
 
-  if (post.thisUserPost == true) {
+  if (post.thisUserPost == true) { //if is the post of the active user
     const crudBtn = $("<button>", "</button>");
     crudBtn.addClass("fas fa-ellipsis-v");
 
@@ -93,18 +95,18 @@ function createNewPost(post) {
     crudOperations.addClass("crud-operations");
     crudOperations.attr("id", "crud-operations");
 
+    //update post
     const updatePost = $("<p>", "</p>");
     updatePost.text("Update post");
     updatePost.addClass("crud-option");
     updatePost.on("click", () => {
-      console.log("post update !");
-
       const updatePopup = $("<div>", "</div>");
       updatePopup.addClass("update-popup");
       updatePopup.attr("id", "update-popup");
 
       const updateDiv = $("<div>", "</div>");
       updateDiv.addClass("update-div");
+      updateDiv.attr("id","update-div");
 
       const message = $("<p>", "</p>");
       message.text("Type here the updated post :");
@@ -114,19 +116,24 @@ function createNewPost(post) {
       const btnUpdate = $("<button>", "</button>");
       btnUpdate.text("Update");
       btnUpdate.on("click", () => {
+        //update post in the database
         console.log("Post updated 2");
       });
 
       const closeBtn = $("<button>", "</button>");
       closeBtn.addClass("fas fa-times");
       closeBtn.on("click", () => {
+        //close update option and DO NOT update post
+        $("#update-div").remove();
         $("#update-popup").remove();
       });
       updateDiv.append(message, newPostInput, btnUpdate, closeBtn);
-      updatePopup.append(updateDiv);
+
       $("#container").append(updatePopup);
+      $("#container").append(updateDiv);
     });
 
+    //delete operation
     const deletePost = $("<p>", "</p>");
     deletePost.text("Delete post");
     deletePost.addClass("crud-option");
@@ -139,6 +146,7 @@ function createNewPost(post) {
 
       const deleteDiv = $("<div>", "</div>");
       deleteDiv.addClass("delete-div");
+      deleteDiv.attr("id","delete-div");
 
       const message = $("<p>", "</p>");
       message.text("Are you sure you want to delete this post ?");
@@ -146,23 +154,27 @@ function createNewPost(post) {
       const btnYes = $("<button>", "</button>");
       btnYes.text("Yes");
       btnYes.on("click", () => {
-        console.log("Post deleted ! ");
+        //delete post from the database
+        $("#delete-div").remove();
         $("#delete-popup").remove();
       });
 
       const btnNo = $("<button>", "</button>");
       btnNo.text("No");
       btnNo.on("click", () => {
-        console.log("Post was NOT deleted ! ");
+        // do NOT delete post from the database
+        $("#delete-div").remove();
         $("#delete-popup").remove();
       });
       deleteDiv.append(message, btnYes, btnNo);
-      deletePopup.append(deleteDiv);
+
       $("#container").append(deletePopup);
+      $("#container").append(deleteDiv);
     });
 
     crudOperations.append(updatePost, deletePost);
 
+    // open/close CRUD options
     let clicks = 0;
     crudBtn.on("click", function () {
       if (clicks == 0) {
@@ -183,7 +195,7 @@ function createNewPost(post) {
     btnAddComm.addClass("btn-comm");
     btnAddComm.text("Add comment");
     btnAddComm.on("click", () => {
-      console.log("Comment added !");
+      //add a new comment
     });
 
     const divPostDetailsBtn = $("<div>", "</div>");
@@ -192,19 +204,19 @@ function createNewPost(post) {
     const btnAllComm = $("<button>", "</button>");
     btnAllComm.addClass("far fa-comment");
     btnAllComm.on("click", () => {
-      console.log("Comments show !");
+      //show all comments
     });
 
     const likeBtn = $("<button>", "</button>");
     likeBtn.addClass("far fa-thumbs-up");
     likeBtn.on("click", () => {
-      console.log("Like added !");
+      //like a post
     });
 
     const savePostBtn = $("<button>", "</button>");
     savePostBtn.addClass("far fa-bookmark");
     savePostBtn.on("click", () => {
-      console.log("Post saved !");
+      //save a post
     });
 
     divPostDetailsBtn.append(savePostBtn, likeBtn, btnAllComm);
