ad56832d221fcc0ee4087e14cab91d9872f492d1
TeodoraMM <teodora_mezei12@yahoo.com>
2020-05-13 09:59:48 +0300

    post new server code

--- server/Posts/post.router.js
+++ server/Posts/post.router.js
@@ -3,50 +3,50 @@ const postsRouter = express.Router();
 const postsDatastore = require('./post.datastore');
 const postsManager = require('./post.manager');
 
+
+postsRouter.route('/all').get(getAll);
+postsRouter.route('/:id').get(getById);
+
+
+postsRouter.route('').post(createPost);
+postsRouter.route('/:id').delete(deletePost);
+postsRouter.route('/:id').put(updatePost);
+
 postsRouter.route('/like/user/:userId/post/:postId').post(manageLikes);
-postsRouter.route('/all').get(getAllPosts);
-postsRouter.route('/:id').get(getPostById);
 
+function getAll(request, response) {
 
-postsRouter.route('').post(makePost);
-postsRouter.route('/id/:id').get(getPosts);
-postsRouter.route('/id/:id').delete(deletePost);
-postsRouter.route('/id/:id').put(putPost);
-
-function makePost(request, response) {
-    const value = request.body;
-    postsDatastore.createPost(value, (data) => {
+    postsDatastore.getAll( (data) => {
         response.status(200).json(data);
     }, (error) => {
         response.status(500).json(error);
     });
 }
 
-function putPost(request, response) {
+function getById(request, response) {
+    const id = request.params.id;
+
+    postsDatastore.getById( id,(data) => {
+        response.status(200).json(data);
+    }, (error) => {
+        response.status(500).json(error);
+    });
+}
+
+function createPost(request, response) {
+    const value = request.body;
+    postsDatastore.create(value, (data) => {
+        response.status(200).json(data);
+    }, (error) => {
+        response.status(500).json(error);
+    });
+}
+
+function updatePost(request, response) {
     const id = request.params.id;
     const value = request.body.text;
 
-    postsDatastore.updatePost(id, value, (data) => {
-        response.status(200).json(data);
-    }, (error) => {
-        response.status(500).json(error);
-    });
-}
-
-function getPosts(request, response) {
-    const id = request.params.id;
-
-    postsDatastore.getAllPosts( id,(data) => {
-        response.status(200).json(data);
-    }, (error) => {
-        response.status(500).json(error);
-    });
-}
-
-function getPostById(request, response) {
-    const id = request.params.id;
-
-    postsDatastore.getPostById( id,(data) => {
+    postsDatastore.update(id, value, (data) => {
         response.status(200).json(data);
     }, (error) => {
         response.status(500).json(error);
@@ -56,7 +56,7 @@ function getPostById(request, response) {
 function deletePost(request, response) {
     const id = request.params.id;
 
-    postsDatastore.removePost(id, (data) => {
+    postsDatastore.remove(id, (data) => {
         response.status(200).json(data);
     }, (error) => {
         response.status(500).json(error);
@@ -73,15 +73,6 @@ function manageLikes(request, response) {
         response.status(500).json(error);
     })
 
-}
-
-function getAllPosts(request, response) {
-
-    postsDatastore.getAll( (data) => {
-        response.status(200).json(data);
-    }, (error) => {
-        response.status(500).json(error);
-    });
 }
 
 
